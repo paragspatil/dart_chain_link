cb103b1ce24dd9ecc0eab400e56378bee88052ee
Parag <paragspatil123@gmail.com>
2019-10-01 13:25:32 +0530

    day 1 dart exercises completed

--- bin/main.dart
+++ bin/main.dart
@@ -7,12 +7,37 @@
 // like this:
 // https://s1.whiteboardfox.com/s/2ae37dc832647bad.png
 
+class Student{
+  String name;
+  int score;
+}
+class Link {
+  String value;
+  Link next;
 
+}
 
 
 main() {
   // Exercise 2:  Create the object structure shown in the above link
   // including the variable called 'firstLink'
+
+  Student student1 = Student();
+  student1.name = 'raj';
+  student1.score = 60;
+  Link Firstlink = Link();
+  Firstlink.value= "mark";
+  Link SecondLink = Link();
+  SecondLink.value = "MAkiko";
+  Firstlink.next = SecondLink;
+  Link ThirdLink = Link();
+  ThirdLink.value = "paul";
+  SecondLink.next = ThirdLink;
+  ThirdLink.next = null;
+  showdata(Firstlink);
+  insert('parag', Firstlink, 1s);
+  print('---------------------');
+  showdata(Firstlink);
 
 }
 
@@ -23,11 +48,56 @@ main() {
 // Raj
 // Makiko
 // Paul
+void showdata(Link t){
+  Link temp = t;
+  while(temp.next!=null) {
+    if (temp.value == null) {
+      print("List is empty");
+    }
+    else {
+      print(temp.value);
+      temp = temp.next;
+      if(temp.next==null){
+        print(temp.value);
+      }
+
+    }
+
+  }
+
+
+
+
+  }
+
+
 
 
 // Exercise 4: Create a function which takes a Link parameter and a String
 // parameter, and adds another link to the end of the chain.  The new link
 // value should be the String parameter passed into the function.typedef
+
+void getLink(String s, Link head) {
+  Link te = head;
+  Link templ = Link();
+
+
+  if(head.next == null){
+    head.value = s;
+    te = te.next;
+  }
+  else{
+
+    templ.value = te.value;
+    templ.next = null;
+    te.value = s;
+
+    te.next = templ;
+    head = te;
+  }
+}
+
+
 
 
 // *** Stretch ****
@@ -36,3 +106,13 @@ main() {
 // in the chain.  For example, calling insert(firstLink, 2, "A") should
 // update the chain structure to look like this:
 // https://s1.whiteboardfox.com/s/77ff03ee231f2f60.png
+void insert(String s, Link head,int j) {
+  Link t = head;
+  int i=0;
+  for(i=1;i<=j;i++){
+    t = t.next;
+  }
+  t.value = s;
+  head = t;
+
+}
